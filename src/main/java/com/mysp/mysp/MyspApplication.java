ff83c02a8a10a879b6cd84739ba5f8522c7a28ae
茂茂 <1724021486@qq.com>
2021-12-19 20:14:01 +0800

    Initial commit

---
+++ src/main/java/com/mysp/mysp/MyspApplication.java
@@ -0,0 +1,12 @@
+package com.mysp.mysp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mysp/mysp/MyspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysp.mysp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
